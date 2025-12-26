83b652ee2804e779aaff0ebc780f8f259b1bba0b
Noelle <111402877+Squibly06@users.noreply.github.com>
2025-12-25 19:43:40 -0500

    Storing Stuff
    Thats usually what I do here

---
+++ VideoDuringSongTest.lua
@@ -0,0 +1,10 @@
+---@diagnostic disable: undefined-global
+function onSongStart()
+    startVideo('ImNightFreddy', 0, false, false, true,false)
+ function onSongStart()
+
+        setSoundVolume('Voices', 0.3)
+         setSoundVolume('Inst', 0.3)
+         addLuaSprite('hud', false)
+ end
+end
